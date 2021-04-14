95625e6cf59a2fad466ae17b477ac9d43de05026
Filippo <filippo.bilardo@tiscali.it>
2021-04-14 12:34:06 +0200

    Update th05-arg_int.c

--- 06-Thread/th05-arg_int.c
+++ 06-Thread/th05-arg_int.c
@@ -20,27 +20,28 @@
  
 //------------------------------------------------------------------------------------------
 void* threadFunc(void* par);
+void* threadFuncAlt(void* par);
 
 //------------------------------------------------------------------------------------------
 int main() {
   
-  pthread_t tid1, tid2, tid3; // Thread id
+  pthread_t tid1, tid2, tid3, tid4; // Thread id
 
   // Data to pass to the thread function
   int data = 123;
-  void* arg = (void*)data;  // Converts to void*
 
   // Creates the thread
-  pthread_create(&tid1, NULL, threadFunc, arg);
+  pthread_create(&tid1, NULL, threadFunc, (void*)&data);
   // Creates the thread
-  pthread_create(&tid2, NULL, threadFunc, (void*)233);
+  void* arg = (void*)data;  // Converts to void*
+  pthread_create(&tid2, NULL, threadFuncAlt, arg);
   // Creates the thread
-  pthread_create(&tid3, NULL, threadFunc, (void*)&data);
+  pthread_create(&tid3, NULL, threadFuncAlt, (void*)533);
   // Creates the thread
-  pthread_create(&tid4, NULL, threadFunc, (void*)'a');
+  pthread_create(&tid4, NULL, threadFuncAlt, (void*)'a');
   
   // Attendo la terminazione dei threads
-  pthread_join(tid1, NULL);
+  //pthread_join(tid1, NULL);
   pthread_join(tid2, NULL);
   pthread_join(tid3, NULL);
   pthread_join(tid4, NULL);
@@ -48,12 +49,25 @@ int main() {
 
 //------------------------------------------------------------------------------------------
 void* threadFunc(void* par) {
-  
-  // Catches the "int" argument. Must know that it is an integer. Type checking is broken
-  int value = (int)par;
+  //int indirizzo = (int *)par;
+  //int valore = *indirizzo;
+  int value = *((int *)par);
   
   // Uses value as an integer
   printf("value = %d\n", value);
+  
+  // Terminates
+  return (void*)0;
+}
+void* threadFuncAlt(void* par) {
+  
+  // Recupero l'argomento "int". Bisogna sapere che è un intero. 
+  // Questo metodo può essere utilizzato solo per passaggio di argomenti di 
+  // tipo intero.
+  int value = (int)par;
+  
+  // Uses value as an integer
+  printf("ThreadID=%d value = %d\n",pthread_self(), value);
   
   // Terminates
   return (void*)0;
