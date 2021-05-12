c18592c36d9dfe9de0b1490feffaf19874c71a9a
Filippo <filippo.bilardo@tiscali.it>
2021-05-12 11:33:09 +0200

    Update blink.c

--- gpio/blink.c
+++ gpio/blink.c
@@ -3,6 +3,11 @@
  *      blinks the first LED
  *      Gordon Henderson, projects@drogon.net
  *
+ * https://www.raspberrypi.org/documentation/usage/gpio/ 
+ * http://wiringpi.com/ 
+ * https://elinux.org/RPi_GPIO_Code_Samples 
+ * https://pinout.xyz/pinout/pin11_gpio17 
+ *
  * gcc -o blink blink.c -lwiringPi
  * sudo ./blink
  */
@@ -12,22 +17,22 @@
 
 int main (void)
 {
-  printf ("Raspberry Pi blink\n") ;
+  printf ("Raspberry Pi blink\n");
 
   if (wiringPiSetup () == -1)
-    return 1 ;
+    return 1;
 
-  pinMode (7, OUTPUT) ;         // aka BCM_GPIO 4
-  pinMode (0, OUTPUT) ;         // aka BCM_GPIO 17
+  pinMode(7, OUTPUT);         // aka BCM_GPIO 4
+  pinMode(0, OUTPUT);         // aka BCM_GPIO 17
 
   for (;;)
   {
-    digitalWrite(7, 1) ;       // On
-    digitalWrite(0, 1) ;       // On
+    digitalWrite(7, 1);       // On
+    digitalWrite(0, 1);       // On
     delay (500);               // mS
-    digitalWrite(7, 0) ;       // Off
-    digitalWrite(0, 0) ;       // Off
+    digitalWrite(7, 0);       // Off
+    digitalWrite(0, 0);       // Off
     delay(500);
   }
-  return 0 ;
+  return 0;
 }
